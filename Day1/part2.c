12bb20ec48c1cddb18b38099b0c41a9c3a1a90ea
Stephen Smith <StephenASmith@protonmail.com>
2019-12-01 13:05:14 -0500

    finished Day 1 Part 2

---
+++ Day1/part2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int calcFuel(int fuel);
+int main(int argc, char *argv[]){
+    FILE *file;
+    int i = 0;
+    unsigned long sum = 0;
+    
+    file = fopen(argv[1], "r");
+    fscanf(file, "%d", &i);
+    
+    while(!feof(file)){
+        int temp = i;
+        i /= 3;         //Why round down when I can just use integer division
+        i -= 2;
+        sum += i;
+        sum += calcFuel(i);
+        fscanf(file, "%d", &i);
+        
+    }
+    
+    fclose (file);
+    printf("%lu\n", sum);
+}
+
+//Recursion seemed the obvious choice for this problem
+int calcFuel(int fuel){
+    int tempFuel = fuel;
+    tempFuel /= 3;
+    tempFuel -= 2;
+    if(tempFuel <= 0){
+        return 0;
+    } else {
+        return tempFuel + calcFuel(tempFuel);
+    }    
+}
+
