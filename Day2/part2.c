45e525629f9ac23c60e13cced97d2ec7e7f51608
Stephen Smith <StephenASmith@protonmail.com>
2019-12-02 18:46:26 -0500

    update comments

--- Day2/part2.c
+++ Day2/part2.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]){
     int *arr = readFile(argv[1], &len);
     int i, j;
     int req = 19690720;
+    //O(n^2) is inefficient, for for only N=100 I figure it's fine
     for(i =0; i< 100 && *arr != req; i++){
         for(j = 0; j < 100 && *arr != req; j++){
             //ensuring the same array every time
@@ -43,7 +44,10 @@ void runProgram(int* program, int len){
          }
     }
 }
-
+/*
+    These add/mul functions aren't strictly required, but I preferred
+    modelling the program like this
+*/
 void add(int in1, int in2, int* out){
     *out = in1 + in2;
 }
@@ -59,7 +63,13 @@ int* readFile(char *argv, int *len){
     int* ptr = (int*) malloc(sizeof(int));
     int index = 0;
 
-    //By using "%d," instead of "%d", we automatically strip the delimiting commas
+    /*
+        fscanf() returns the number of elements scanned, then moves the file pointer
+        we only want to break the loop when it stops reciving valid input, or, when it
+        returns any number other than 1
+      
+        By using "%d," instead of "%d" we automatically strip the delimiting commas  
+    */
     while(fscanf(fp, "%d,", ptr+index) == 1){
         index++;
         ptr = (int*) realloc(ptr, (index+1)*sizeof(int));
