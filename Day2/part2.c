a559daca1dca9b2c732e0047ed564abb9faa40e4
Stephen Smith <StephenASmith@protonmail.com>
2019-12-02 15:22:38 -0500

    finish part 2

---
+++ Day2/part2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int* readFile(char argv[], int *len);
+void runProgram(int* program, int len);
+void add(int* in1, int* in2, int* out);
+void mul(int* in1, int* in2, int* out);
+
+int main(int argc, char *argv[]){
+    int len;
+    int *arr = readFile(argv[1], &len);
+    int i, j;
+    int req = 19690720;
+    for(i =0; i< 100 && *arr != req; i++){
+        for(j = 0; j < 100 && *arr != req; j++){
+            //ensuring the same array every time
+            arr = readFile(argv[1], &len);
+            *(arr+1) = i;
+            *(arr+2) = j;
+            runProgram(arr, len);
+        }
+    }
+    //To negate the final increment the for loop does before terminating
+    i--;
+    j--;
+    printf("%d\nI: %d\tJ: %d\n", 100 * i + j, i, j);
+}
+
+void runProgram(int* ip, int len){
+    int i;
+    for(i = 0; i < len; i+=4){
+        switch(*(ip+i)){
+            case 1:
+                add(ip + *(ip+i+1), ip + *(ip+i+2), ip + *(ip+i+3));
+                break;
+            case 2:
+                mul(ip + *(ip+i+1), ip + *(ip+i+2), ip + *(ip+i+3));
+                break;
+            case 99:
+                return;
+            default:
+                return;
+         }
+    }
+}
+
+void add(int* in1, int* in2, int* out){
+    *out = *in1 + *in2;
+}
+
+void mul(int* in1, int* in2, int* out){
+    *out = *in1 * *in2;
+}
+
+int* readFile(char *argv, int *len){
+    FILE *fp;
+    fp = fopen(argv, "r");
+    
+    int* ptr = (int*) malloc(sizeof(int));
+    int index = 0;
+    short terminate = 0;
+
+    while(terminate == 0){
+        if(fscanf(fp, "%d,", ptr+index) == 1){
+            index++;
+            ptr = (int*) realloc(ptr, (index+1)*sizeof(int));
+        } else {
+            terminate = 1;
+        }
+    }
+    fclose(fp);
+    
+    *len = index;
+    return ptr;
+}
