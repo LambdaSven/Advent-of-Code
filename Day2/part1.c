a471d076421ce192f3e948ae022143ba542532d7
Stephen Smith <StephenASmith@protonmail.com>
2019-12-02 13:35:22 -0500

    complete part 1

--- Day2/part1.c
+++ Day2/part1.c
@@ -1,37 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-int* readFile(char argv[]);
+int* readFile(char argv[], int *len);
+void runProgram(int* program, int len);
+void add(int* in1, int* in2, int* out);
+void mul(int* in1, int* in2, int* out);
+
 
 int main(int argc, char *argv[]){
-    int *arr = readFile(argv[1]);
-    int len = sizeof(arr) / sizeof(int);
+    int len;
+    int *arr = readFile(argv[1], &len);
     int i;
-    puts("\n");
-    for(i = 0; i < len; i++){
-        printf("%d, ", arr[i]);
+    *(arr+1) = 12;
+    *(arr+2) = 2;    
+    runProgram(arr, len);
+    printf("%d\n", *arr);
+}
+
+void runProgram(int* ip, int len){
+    int i;
+    for(i = 0; i < len; i+=4){
+        switch(*(ip+i)){
+            case 1:
+                add(ip + *(ip+i+1), ip + *(ip+i+2), ip + *(ip+i+3));
+                break;
+            case 2:
+                mul(ip + *(ip+i+1), ip + *(ip+i+2), ip + *(ip+i+3));
+                break;
+            case 99:
+                return;
+         }
     }
 }
 
-int* readFile(char *argv){
+void add(int* in1, int* in2, int* out){
+    *out = *in1 + *in2;
+}
+
+void mul(int* in1, int* in2, int* out){
+    *out = *in1 * *in2;
+}
+
+int* readFile(char *argv, int *len){
     FILE *fp;
     fp = fopen(argv, "r");
     
-    // big malloc so that I can ensure enough space
-    int* ptr = (int*) malloc(500 * sizeof(int));
+    int* ptr = (int*) malloc(sizeof(int));
     int index = 0;
-    while(fscanf(fp, "%d", ptr+index) == 1 || fscanf(fp, "%c", ptr+index) == 1){
-        printf("%d, ", *(ptr + index));
-        index++;
+    short terminate = 0;
+    char garbage;
+
+    while(terminate == 0){
+        if(fscanf(fp, "%d", ptr+index) == 1){
+            index++;
+            ptr = (int*) realloc(ptr, (index+1)*sizeof(int));
+        } else if(fscanf(fp, "%c", &garbage ) != 1){
+            terminate = 1;
+        }
     }
-        
-    printf("\n INDEX: %d", index);    
-    // don't need calloc since I should be setting every element successfully
-    int* arr = (int*) malloc(index * sizeof(int));
-    int len = index;
-    for(index = 0; index < len/2; index++){
-        *(arr + index) = *(ptr + index*2);
-    }
-    free(ptr);
-    fclose(fp)
-    return arr;
+    fclose(fp);
+    
+    *len = index;
+    return ptr;
 }
