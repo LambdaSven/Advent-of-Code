56221a7a894b3e1f2cc5e4c4dee0744bd69cdd28
Stephen Smith <stephenasmith@protonmail.com>
2019-12-03 17:02:53 -0500

    complete day 3

--- Day3/part2.c
+++ Day3/part2.c
@@ -5,6 +5,13 @@ struct point{
     int x;
     int y;
     int isInt;
+    int stepsToPoint;
+};
+struct intersection{
+    int x;
+    int y;
+    int stepsw1;
+    int stepsw2;
 };
 
 int main(int argc, char* argv[]){
@@ -15,24 +22,29 @@ int main(int argc, char* argv[]){
     (*points).y = 0;
     (*points).isInt = 1;
 
+    struct intersection* ints = malloc(sizeof(*ints));
     char ins;
     int len;
     int numStructs = 1;
+    int numInts = 0;
     //RUN
     int wire = 1;
     int x = 0;
     int y = 0;
+    int currentSteps = 0;
     while(fscanf(fp, "%c", &ins) == 1){
         if (ins == '\n'){
             puts("***************\n*RESET*\n***************");
             x = 0;
             y = 0;
+            currentSteps = 0;
             wire++;
         }else{
             fscanf(fp, "%d,", &len);
             printf("%c%d,", ins, len);   
             int i;
             for(i = 0; i < len; i++){
+                currentSteps++;
                 switch(ins){
                     case 'R':
                         x++;
@@ -53,6 +65,14 @@ int main(int argc, char* argv[]){
                     if(points[j].x == x && points[j].y == y){
                         isDupe = 1;
                         points[j].isInt += wire;
+                        if(points[j].isInt == 3){
+                            numInts++;
+                            ints = realloc(ints, numInts*sizeof(*ints));
+                            ints[numInts-1].x = x;
+                            ints[numInts-1].y = y;
+                            ints[numInts-1].stepsw1 = points[j].stepsToPoint;
+                            ints[numInts-1].stepsw2 = currentSteps;
+                        }
                     }
                 }
                 if(isDupe == 0){
@@ -62,6 +82,7 @@ int main(int argc, char* argv[]){
                     points[numStructs-1].x = x;
                     points[numStructs-1].y = y;
                     points[numStructs-1].isInt = wire;
+                    points[numStructs-1].stepsToPoint = currentSteps;
                 }
 
             }
@@ -81,4 +102,13 @@ int main(int argc, char* argv[]){
         }
     }
     printf("FINAL DISTANCE: %d\n", final_dist);
-} 
+    
+    long steps = 99999999;
+    for(i = 0; i < numInts; i++){
+        int tempSteps = ints[i].stepsw1 + ints[i].stepsw2;
+        if(steps > tempSteps){
+            steps = tempSteps;
+        }
+    } 
+    printf("SHORTEST PATH: %d\n", steps);
+}
