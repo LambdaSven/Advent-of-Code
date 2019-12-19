f77cda147e410f6dd936c81e484428e4c85e351d
Stephen Smith <StephenASmith@protonmail.com>
2019-12-18 19:31:18 -0500

    kept trying day 14

--- C/Day14/part1.c
+++ C/Day14/part1.c
@@ -78,6 +78,7 @@ int main(int argc, char* argv[]){
     int finish = 0;
     int fuelAmount;
     int extra = 0;
+    int sumExtra = 0;
     for(i = 0; finish == 0; i = (i+1)%numReactions){
         solvable = 1;
         //check if every element in the equation has a proper value
@@ -88,19 +89,32 @@ int main(int argc, char* argv[]){
         }
     //if solvable and not already solved
     if(solvable == 1 && allReactions[i].output.numOre == 0){
+        printf("*** NEW ELEMENT ***\n");
         int numOres = 0;
         int temp = 0;
         //cycle through all inputs
         for(j = 0; j < allReactions[i].numElements; j++){
             for(k = 0; temp <= allReactions[i].input[j].numOre * allReactions[i].inAmounts[j];k++){
-                temp = k * allReactions[i].input[j].minProduced;
+                temp = k * allReactions[i].input[j].minProduced + extra;
             }
+            //If the extra was nessecary
+            if(temp-extra < allReactions[i].input[j].numOre*allReactions[i].inAmounts[j]){
+                //should subtract the extra used from extra
+                extra += temp - extra - allReactions[i].input[j].numOre*allReactions[i].inAmounts[j];
+                printf("LOST EXTRA\t");
+            //didn't use any extra
+            } else {
+                extra += temp - allReactions[i].input[j].numOre*allReactions[i].inAmounts[j];
+                printf("GAINED EXTRA\t");
+            }
+            temp = allReactions[i].input[j].numOre*allReactions[i].inAmounts[j];
             numOres += temp/allReactions[i].outAmount;
-            extra += temp - allReactions[i].input[j].numOre * allReactions[i].inAmounts[j];
             printf("EXTRA: %d\n", extra);
         }
+        sumExtra += extra;
         allReactions[i].output.numOre = numOres;
         allReactions[i].output.minProduced = allReactions[i].outAmount;
+        printf("ID %s COSTS %d\n", allReactions[i].output.id, allReactions[i].output.numOre);
 
         for(j = 0; j < numReactions; j++){
             for(k = 0; k < allReactions[j].numElements; k++){
@@ -116,7 +130,7 @@ int main(int argc, char* argv[]){
         }
     }
 }    
-    printf("FUEL: %d\tEXTRA: %d\n", fuelAmount, extra);
+    printf("FUEL: %d\tEXTRA: %d SUMEXTRA: %d\n", fuelAmount, extra, sumExtra);
 }
 
 
