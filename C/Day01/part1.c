0f295f1eba2ac6dd0a856206dea0b3ea30aeda21
Stephen Smith <StephenASmith@protonmail.com>
2020-07-23 13:08:11 -0400

    add handling day 1

--- C/Day01/part1.c
+++ C/Day01/part1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char *argv[]){
+    if(argc != 2){
+        fprintf(stderr, "You must include exactly 1 input files.\n");
+        exit(-1);
+    }
     FILE *file;
     int i = 0;
     unsigned long sum = 0;
