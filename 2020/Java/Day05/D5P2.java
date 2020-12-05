0a6fbb2c71b6f967d66d8ffbc65922aa148c78c4
Stephen Smith <stephenasmith@protonmail.com>
2020-12-05 11:39:24 -0500

    remove unecessay intermediary step

--- 2020/Java/Day05/D5P2.java
+++ 2020/Java/Day05/D5P2.java
@@ -2,21 +2,20 @@ package Day05;
 
 import aocutil.Utilities;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class D5P2 {
   public static void main(String[] args){
-    ArrayList<BoardingPass> manifest = Utilities.parseInputFile(new File(args[0]))
-                          .stream()
-                          .map(BoardingPass::new)
-                          .collect(Collectors.toCollection(ArrayList::new));
-    
-    HashMap<Integer, BoardingPass> map = new HashMap<Integer, BoardingPass>();
-    for(BoardingPass e : manifest){
-      map.put(e.getChecksum(), e);
-    }
+    HashMap<Integer, BoardingPass> map = Utilities
+    .parseInputFile(new File(args[0]))
+    .stream()
+    .map(BoardingPass::new)
+    .collect(Collectors.toMap(BoardingPass::getChecksum, //Integer
+                               Function.identity(),      //BoardingPass
+                               (k1, K2) -> k1,           //Merging fn - resolves collisions
+                               HashMap::new));
     
     //seats at front and back don't exist means our 
     //ID range is likely 1 * 8 + 0 = 8
