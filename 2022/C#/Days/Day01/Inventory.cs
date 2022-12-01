67d121f6c13e8e8e87e5826210682f95937d3523
Stephen Smith <Stephen.Smith@D2L.com>
2022-12-01 10:38:32 -0500

    remove unnecessary constructor

--- 2022/C#/Days/Day01/Inventory.cs
+++ 2022/C#/Days/Day01/Inventory.cs
@@ -3,16 +3,6 @@ namespace nDay01
     public class Inventory
     {
         List<int> backpack;
-        public Inventory(List<String> l)
-        {
-            backpack = new();
-
-            foreach(String s in l)
-            {
-                backpack.Add(Int32.Parse(s));
-            }
-        }
-
         public Inventory()
         {
             backpack = new();
